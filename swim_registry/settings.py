f06e5db1553beeb0d1356574831f0ec3ccd952ee
iuriramos <iuri.srb@gmail.com>
2017-02-27 19:15:42 -0300

    Change staticfiles settings

--- swim_registry/settings.py
+++ swim_registry/settings.py
@@ -107,8 +107,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
@@ -120,4 +118,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
