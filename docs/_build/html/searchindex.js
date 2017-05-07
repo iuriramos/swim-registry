1015461ef636202674c02f9eda16a2f0fd288df1
iuriramos <iuri.srb@gmail.com>
2017-05-07 18:37:53 -0300

    Add documentation support

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to SWIM Registry&#8217;s documentation!"],titleterms:{document:0,indic:0,registri:0,swim:0,tabl:0,welcom:0}})
