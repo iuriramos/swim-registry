4b13a5032829bdd7e3dec36da61ac3184722c195
iuriramos <iuri.srb@gmail.com>
2017-05-25 20:50:18 -0300

    Add website attribute

--- community/models/participant.py
+++ community/models/participant.py
@@ -5,6 +5,7 @@ from swim_registry.models import TimeStampedModel
 class Participant(TimeStampedModel):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    website = models.URLField(blank=True)
     category = models.ForeignKey('community.ParticipantCategory', related_name='participants')
     image = models.ImageField(upload_to = 'participants/profiles/images/', default = 'participants/profiles/images/none/default.svg')
     reviewed = models.BooleanField(default=False)
